ebc3668526a9eecec20eaa3e5cd023914e601b6c
dangling2300 <dangling2300@proton.me>
2026-05-11 03:47:37 +0000

    Create signal.go

---
+++ server/signal.go
@@ -0,0 +1,28 @@
+//go:build linux || darwin || freebsd
+// +build linux darwin freebsd
+
+package main
+
+import (
+	"log"
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+func init() {
+	go sigHandler()
+}
+
+func sigHandler() {
+	ch := make(chan os.Signal, 1)
+	signal.Notify(ch, syscall.SIGUSR1)
+	signal.Ignore(syscall.SIGPIPE)
+
+	for {
+		switch <-ch {
+		case syscall.SIGUSR1:
+			log.Printf("SNMP:")
+		}
+	}
+}
